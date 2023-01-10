01a4d29444fd1673f5b37ce2f22eb5e82375d7d0
hector <hpulido1021@gmail.com>
2023-01-10 14:13:06 -0500

    feature: Button component

---
+++ src/components/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#4F63AC',
+    padddingVertical: 10,
+    paddingTop: 10,
+    paddingHorizontal: 8,
+    borderRadius: 12,
+    width: '100%',
+    height: '6%',
+  },
+  title: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import {Text, Image, View} from 'react-native';
+import {Text, Image, View, Pressable} from 'react-native';
+import Button from '../../../components/Button';
 import {styles} from './styles';
 
 const Splash = () => {
   return (
-    <View>
-      <Image style={styles.image} source={require('./splash_image.png')} />
-      <Text>You'll find All you need here!</Text>
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require('./splash_image.png')}
+      />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}> You'll find </Text>
+        <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
+        <Text style={styles.title}> Here! </Text>
+      </View>
+      <Button title="Sing Up" />
+      <Pressable hitSlop={20}>
+        <Text style={styles.footerText}> Sing in</Text>
+      </Pressable>
     </View>
   );
 };
